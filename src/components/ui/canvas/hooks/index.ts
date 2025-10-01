336fd757702b6259cb5dfa762a46782e36482ed6
Ben Steinher <130510994+human-bee@users.noreply.github.com>
2025-10-01 16:59:38 -0700

    refactor: extract canvas interactions hooks

--- src/components/ui/canvas/hooks/index.ts
+++ src/components/ui/canvas/hooks/index.ts
@@ -2,3 +2,4 @@ export * from './useCanvasComponentStore';
 export * from './useCanvasRehydration';
 export * from './useCanvasThreadReset';
 export * from './useCanvasEvents';
+export * from './useCanvasInteractions';
